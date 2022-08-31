06a1830580836ffbee0848e3c7e598027802dd66
유정주 <dbwjdwn9216@gmail.com>
2022-08-31 20:23:51 +0900

    Add: Home VC 추가

---
+++ TLDR/TLDR/HomeScreen/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TLDR
+//
+//  Created by 유정주 on 2022/08/31.
+//
+
+import UIKit
+
+final class HomeView: UIView {
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
---
+++ TLDR/TLDR/HomeScreen/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TLDR
+//
+//  Created by 유정주 on 2022/08/31.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController<HomeView> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
