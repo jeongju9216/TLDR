5e54b31a91b21a99fb4d6c05ddf997c0af5748f6
유정주 <dbwjdwn9216@gmail.com>
2022-08-31 20:27:27 +0900

    Add: SizeUtil 추가

---
+++ TLDR/TLDR/Utils/SizeUtil.swift
@@ -0,0 +1,25 @@
+//
+//  SizeUtil.swift
+//  GardenOfTranslation
+//
+//  Created by 유정주 on 2022/08/24.
+//
+
+import UIKit
+
+final class SizeUtil {
+    /**
+     statusbar 높이 구하기
+     */
+    static var statusBarHeight: CGFloat {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        
+        let statusBarHeight: CGFloat = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        
+        print("\(#line)-line, \(#function)")
+        return statusBarHeight
+    }
+    
+}
