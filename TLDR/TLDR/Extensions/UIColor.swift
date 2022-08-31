a6cf1cfcc7ff207bd543233db3367208e77ed3cf
유정주 <dbwjdwn9216@gmail.com>
2022-08-31 19:37:46 +0900

    Add: UIColor extension 추가

---
+++ TLDR/TLDR/Extensions/UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor.swift
+//  TLDR
+//
+//  Created by 유정주 on 2022/08/31.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundColor: UIColor {
+        return UIColor(named: "BackgroundColor") ?? .black
+    }
+    
+    static var mainColor: UIColor {
+        return UIColor(named: "MainColor") ?? .black
+    }
+
+    static var darkColor: UIColor {
+        return UIColor(named: "DarkColor") ?? .black
+    }
+}
