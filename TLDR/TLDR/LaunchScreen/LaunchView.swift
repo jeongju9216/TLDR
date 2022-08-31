bd6bb9a009d4676a168542f2a78aa151d29529b3
유정주 <dbwjdwn9216@gmail.com>
2022-08-31 19:37:58 +0900

    Add: LaunchView 추가

---
+++ TLDR/TLDR/LaunchScreen/LaunchView.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchView.swift
+//  TLDR
+//
+//  Created by 유정주 on 2022/08/31.
+//
+
+import UIKit
+
+final class LaunchView: UIView {
+    
+    //MARK: - Views
+    
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: - Setup
+    private func setup() {
+        
+    }
+}
