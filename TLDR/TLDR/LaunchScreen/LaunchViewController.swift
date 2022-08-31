de76e5a80d1e5407ac6587b6749f4c4250d8435e
유정주 <dbwjdwn9216@gmail.com>
2022-08-31 19:38:07 +0900

    Add: 런치 VC 추가

---
+++ TLDR/TLDR/LaunchScreen/LaunchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LauchViewController.swift
+//  TLDR
+//
+//  Created by 유정주 on 2022/08/31.
+//
+
+import UIKit
+
+final class LaunchViewController: BaseViewController<LaunchView> {
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
