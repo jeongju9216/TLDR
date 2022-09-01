b934123c15c3f5b51207521bacea7e532262434a
유정주 <dbwjdwn9216@gmail.com>
2022-09-01 09:09:02 +0900

    Add: Hide Layout 추가

--- TLDR/TLDR/HomeScreen/HomeViewController.swift
+++ TLDR/TLDR/HomeScreen/HomeViewController.swift
@@ -13,10 +13,34 @@ final class HomeViewController: BaseViewController<HomeView> {
         super.viewDidLoad()
         
         self.layoutView.sumUpButton.addTarget(self, action: #selector(clickedSumUpButton), for: .touchUpInside)
+        self.layoutView.hideKeyboardButton.addTarget(self, action: #selector(clickedHideKeyboardButton), for: .touchUpInside)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification , object:nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification , object:nil)
     }
     
     //MARK: - Actions
     @objc func clickedSumUpButton() {
         print("\(#line)-line, \(#function)")
+    }
+    
+    @objc func clickedHideKeyboardButton() {
+        print("\(#line)-line, \(#function)")
+        self.layoutView.showTopBar()
+        self.layoutView.showSumUpButton()
+        
+        self.layoutView.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        print("\(#line)-line, \(#function)")
+        self.layoutView.hideTopBar()
+        self.layoutView.hideSumUpButton()
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        print("\(#line)-line, \(#function)")
+        
     }
 }
