dd497882628cf9d88024d8f0aa9057e36ce03d7f
유정주 <dbwjdwn9216@gmail.com>
2022-09-01 09:08:49 +0900

    Add: Hide Layout 추가

--- TLDR/TLDR/HomeScreen/HomeView.swift
+++ TLDR/TLDR/HomeScreen/HomeView.swift
@@ -17,11 +17,14 @@ final class HomeView: UIView {
     
     var sumUpButton: UIButton!
     
+    var hideKeyboardButton: UIButton!
+    
     //MARK: - Properties
     private var statusBarHeight: CGFloat = SizeUtil.statusBarHeight
     private var topBarHeight: CGFloat {
         return 50 + statusBarHeight
     }
+    private var animationDuration: Double = 0.3
     
     //MARK: - Init
     override init(frame: CGRect) {
@@ -34,19 +37,62 @@ final class HomeView: UIView {
         setup()
     }
     
+    //MARK: - Methods
+    func showTopBar() {
+        self.textView.textContainerInset = UIEdgeInsets(top: 60, left: 15, bottom: 10, right: 15)
+        
+        UIView.animate(withDuration: animationDuration, animations: {
+            let scale = CGAffineTransform(translationX: 0, y: 0)
+            self.topBarView.transform = scale
+            
+            self.topBarView.alpha = 1
+        })
+    }
+    
+    func hideTopBar() {
+        self.textView.textContainerInset = UIEdgeInsets(top: 20, left: 15, bottom: 250, right: 15)
+
+        UIView.animate(withDuration: animationDuration, animations: {
+            let scale = CGAffineTransform(translationX: 0, y: -(self.topBarHeight))
+            self.topBarView.transform = scale
+            
+            self.topBarView.alpha = 0
+        })
+    }
+    
+    func showSumUpButton() {
+        UIView.animate(withDuration: animationDuration, animations: {
+            let scale = CGAffineTransform(translationX: 0, y: 0)
+            self.sumUpButton.transform = scale
+            
+            self.sumUpButton.alpha = 1
+        })
+    }
+    
+    func hideSumUpButton() {
+        UIView.animate(withDuration: animationDuration, animations: {
+            let scale = CGAffineTransform(translationX: 0, y: 80)
+            self.sumUpButton.transform = scale
+            
+            self.sumUpButton.alpha = 0
+        })
+    }
+    
     //MARK: - Setup
     private func setup() {
         setupTopBarView()
         setupTitleLable()
-        setupTextView()
+        
         setupSumUpButton()
+        setupHideKeyboardButton()
+        
+        setupTextView()
         
         self.bringSubviewToFront(topBarView)
     }
     
     private func setupTopBarView() {
-        topBarView = UIView(frame: .init(x: 0, y: 0,
-                                        width: self.bounds.width, height: topBarHeight))
+        topBarView = UIView(frame: .init(x: 0, y: 0, width: self.bounds.width, height: topBarHeight))
         
         topBarView.backgroundColor = .darkColor
                 
@@ -66,11 +112,17 @@ final class HomeView: UIView {
     
     private func setupTextView() {
         textView = UITextView()
+        textView.backgroundColor = .backgroundColor
+        
+        textView.font = UIFont.systemFont(ofSize: 18)
+        textView.textContainerInset = UIEdgeInsets(top: 60, left: 15, bottom: 10, right: 15)
+        
+        textView.inputAccessoryView = hideKeyboardButton
         
         self.addSubview(textView)
         textView.pinWidth(constant: self.bounds.width)
-        textView.pinTop(to: self.topBarView.bottomAnchor)
-        textView.pinBottom(to: self.safeAreaLayoutGuide.bottomAnchor)
+        textView.pinTop(to: self.safeAreaLayoutGuide.topAnchor)
+        textView.pinBottom(to: self.sumUpButton.topAnchor)
     }
     
     private func setupSumUpButton() {
@@ -83,11 +135,28 @@ final class HomeView: UIView {
         sumUpButton.setTitleColor(.lightGray, for: .highlighted)
         
         sumUpButton.titleLabel?.textAlignment = .center
-        sumUpButton.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        sumUpButton.titleLabel?.font = .boldSystemFont(ofSize: 21)
         
         self.addSubview(sumUpButton)
         sumUpButton.pinWidth(constant: self.bounds.width)
         sumUpButton.pinHeight(constant: 80)
         sumUpButton.pinBottom(to: self.bottomAnchor)
+    }
+    
+    private func setupHideKeyboardButton() {
+        hideKeyboardButton = UIButton(type: .custom)
+        
+        hideKeyboardButton.backgroundColor = .backgroundColor
+        
+        hideKeyboardButton.setTitle("⌵", for: .normal)
+        hideKeyboardButton.setTitleColor(.label, for: .normal)
+        
+//        hideKeyboardButton.titleLabel?.textAlignment = .center
+        hideKeyboardButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 0, bottom: 20, right: 0)
+        hideKeyboardButton.titleLabel?.font = .boldSystemFont(ofSize: 32)
+        
+        self.addSubview(hideKeyboardButton)
+        hideKeyboardButton.pinWidth(constant: self.bounds.width)
+        hideKeyboardButton.pinHeight(constant: 20)
     }
 }
