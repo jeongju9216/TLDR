c4dc57bb94b6fc9546c49f40145c6bb3e708b817
유정주 <dbwjdwn9216@gmail.com>
2022-09-22 09:01:27 +0900

    Add: 앱 버전 통신 추가

--- TLDR/TLDR/LaunchScreen/LaunchViewController.swift
+++ TLDR/TLDR/LaunchScreen/LaunchViewController.swift
@@ -14,11 +14,29 @@ final class LaunchViewController: BaseViewController<LaunchView> {
         super.viewDidLoad()
         
         Task {
+            let versionResponse = await HttpService.shard.getVersion()
+            if versionResponse.result == .ok, let versionData = versionResponse.data {
+                setVersion(versionData)
+            }
+            
             try await Task.sleep(nanoseconds: TimeUtil.nano2sec(0.5)) //런치 시간
                     
             let navigationVC: UINavigationController = UINavigationController(rootViewController: HomeViewController())
             navigationVC.modalPresentationStyle = .fullScreen
             self.present(navigationVC, animated: false)
         }
+    }
+    
+    private func setVersion(_ data: String) {
+        BaseData.shared.currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+        
+    }
+    
+    private func postVersion() async {
+        let forced: String = "1.0.0"
+        let lasted: String = "1.0.0"
+        
+        let result = await HttpService.shard.postVersion(forced: forced, lasted: lasted)
+        print(result)
     }
 }
