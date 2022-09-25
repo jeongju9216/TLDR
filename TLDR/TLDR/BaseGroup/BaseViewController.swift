ff97da826740f76aaa29e28912fd98b8d6c6d0cf
유정주 <dbwjdwn9216@gmail.com>
2022-09-25 21:18:30 +0900

    Fix: Error Alert에 action 버튼 추가

--- TLDR/TLDR/BaseGroup/BaseViewController.swift
+++ TLDR/TLDR/BaseGroup/BaseViewController.swift
@@ -13,9 +13,9 @@ class BaseViewController<LayoutView: UIView>: UIViewController {
         return view as! LayoutView
     }
     
+    private let errorMessage = "에러가 발생했습니다.\n다시 시도해 주세요."
     private var errorAlert: UIAlertController = {
-        let errorMessage = "에러가 발생했습니다.\n다시 시도해 주세요."
-        let alert = UIAlertController(title: "에러", message: errorMessage, preferredStyle: .alert)
+        let alert = UIAlertController(title: "에러", message: "", preferredStyle: .alert)
         return alert
     }()
     
@@ -37,13 +37,15 @@ class BaseViewController<LayoutView: UIView>: UIViewController {
         self.navigationItem.scrollEdgeAppearance = appearance
     }
     
-    func showErroAlert(message: String? = nil) {
+    func showErroAlert(message: String? = nil, action: ((UIAlertAction) -> Void)? = nil) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
+
+            let message = message ?? self.errorMessage
+            self.errorAlert.message = message
             
-            if let message = message {
-                self.errorAlert.message = message
-            }
+            let doneAction = UIAlertAction(title: "확인", style: .default, handler: action)
+            self.errorAlert.addAction(doneAction)
             
             self.present(self.errorAlert, animated: true)
         }
