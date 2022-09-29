e1a06020ba24a7fd5c3b3fa80c51e01d0582655c
유정주 <dbwjdwn9216@gmail.com>
2022-09-29 15:15:38 +0900

    Add: init 추가

--- TLDR/TLDR/Models/SummarizeData.swift
+++ TLDR/TLDR/Models/SummarizeData.swift
@@ -10,7 +10,19 @@ import Foundation
 struct SummarizeData {
     let text: String //원본
     let summarizeText: String //요약 내용
-    let keywords: Set<String> //핵심 키워드
+    let keywords: [String] //핵심 키워드
+    
+    init() {
+        self.text = ""
+        self.summarizeText = ""
+        self.keywords = []
+    }
+    
+    init(text: String, summarizeText: String, keywords: [String]) {
+        self.text = text
+        self.summarizeText = summarizeText
+        self.keywords = keywords
+    }
 }
 
 struct SummarizeResponseData: Decodable {
@@ -20,7 +32,7 @@ struct SummarizeResponseData: Decodable {
 }
 
 enum SummarizeLangauge: String {
-    case auto
-    case en
-    case ko
+    case auto //서버에서 자동 인식
+    case en //영어로 번역
+    case ko //한국어로 번역
 }
