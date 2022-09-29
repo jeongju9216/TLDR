4bcd5ab19c140fce6270fe29547dc9046eaa219a
유정주 <dbwjdwn9216@gmail.com>
2022-09-29 11:44:55 +0900

    Fix: Launch 버전 체크 수정

--- TLDR/TLDR/LaunchScreen/LaunchViewController.swift
+++ TLDR/TLDR/LaunchScreen/LaunchViewController.swift
@@ -16,10 +16,13 @@ final class LaunchViewController: BaseViewController<LaunchView> {
         Logger.debug("앱 시작")
         Task {
             let testJson = "{\"forced\":\"0.0.1\", \"lasted\":\"0.0.1\", \"appleID\" : \"123456\", \"bundleID\" : \"com.jeong9216.TLDR\"}"
-            let versionResponse = Response(result: .ok, message: "테스트", data: testJson)//await HttpService.shard.getVersion()
+            let versionResponse = Response(result: .ok, message: "테스트", data: testJson)
+            //let versionResponse = await HttpService.shard.getVersion()
             
             do {
-                guard versionResponse.result == .ok else { throw HttpError.apiError }
+                guard versionResponse.result == .ok else {
+                    throw HttpError.apiError
+                }
                 
                 let versionData = try parsingVersionData(versionResponse)
                 setVersion(versionData)
@@ -29,7 +32,9 @@ final class LaunchViewController: BaseViewController<LaunchView> {
                 goHomeVC()
             } catch {
                 Logger.error(error.localizedDescription)
-                self.showErroAlert()
+                self.showErroAlert(action: {_ in
+                    exit(0)
+                })
             }
         }
     }
@@ -43,12 +48,12 @@ final class LaunchViewController: BaseViewController<LaunchView> {
         }
 
         let versionData = try JSONDecoder().decode(VersionData.self, from: data)
-        Logger.info(versionData)
-        
         return versionData
     }
     
     private func setVersion(_ version: VersionData) {
+        Logger.info(version)
+        
         BaseData.shared.currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
         
         BaseData.shared.forcedUpdateVersion = version.forced
@@ -61,8 +66,6 @@ final class LaunchViewController: BaseViewController<LaunchView> {
     private func goHomeVC() {
         let navigationVC: UINavigationController = UINavigationController(rootViewController: HomeViewController())
         navigationVC.modalPresentationStyle = .fullScreen
-
-        self.dismiss(animated: false)
         
         self.present(navigationVC, animated: false)
     }
