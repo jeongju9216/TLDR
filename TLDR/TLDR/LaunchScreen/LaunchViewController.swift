bdd815a14f16e344e85f065fad8db6c2ff85967c
유정주 <dbwjdwn9216@gmail.com>
2022-10-02 22:35:41 +0900

    Fix: 서버 GET 테스트

--- TLDR/TLDR/LaunchScreen/LaunchViewController.swift
+++ TLDR/TLDR/LaunchScreen/LaunchViewController.swift
@@ -14,6 +14,7 @@ final class LaunchViewController: BaseViewController<LaunchView> {
         super.viewDidLoad()
         
         Logger.debug("앱 시작")
+        
         launch()
     }
     
@@ -21,10 +22,16 @@ final class LaunchViewController: BaseViewController<LaunchView> {
     private func launch() {
         Task {
             let testJson = "{\"forced\":\"0.0.1\", \"lasted\":\"0.0.1\", \"appleID\" : \"123456\", \"bundleID\" : \"com.jeong9216.TLDR\"}"
-            let versionResponse = Response(result: .ok, message: "테스트", data: testJson)
-            //let versionResponse = await HttpService.shard.getVersion()
+//            let versionResponse = Response(result: .ok, message: "테스트", data: testJson)
+            let stateResponse = await HttpService.shard.getState()
+            let versionResponse = await HttpService.shard.getVersion()
             
             do {
+                let stateData = try parsingStateData(stateResponse)
+                guard stateResponse.result == .ok, stateData.state == .ok else {
+                    throw HttpError.serverStateError(notice: stateData.notice)
+                }
+                
                 guard versionResponse.result == .ok else {
                     throw HttpError.apiError
                 }
@@ -35,24 +42,38 @@ final class LaunchViewController: BaseViewController<LaunchView> {
                 try await Task.sleep(nanoseconds: TimeUtil.nano2sec(0.5)) //런치 시간
                 
                 goHomeVC()
+            } catch HttpError.serverStateError(let notice) {
+                Logger.error(notice)
+                self.showErroAlert(message: notice, action: { _ in
+                    exit(0)
+                })
             } catch {
-                Logger.error(error.localizedDescription)
-                self.showErroAlert(action: {_ in
+                Logger.error("\(error) / \(error.localizedDescription)")
+                self.showErroAlert(action: { _ in
                     exit(0)
                 })
             }
         }
     }
     
+    private func parsingStateData(_ response: Response) throws -> StateData {
+        Logger.debug(response.data)
+
+        guard let json = response.data else {
+            throw HttpError.jsonError
+        }
+        
+        return try StateData.decode(dictionary: json)
+    }
+    
     private func parsingVersionData(_ response: Response) throws -> VersionData {
         Logger.debug(response.data)
 
-        guard let json = response.data, let data = json.data(using: .utf8) else {
+        guard let json = response.data else {
             throw HttpError.jsonError
         }
-
-        let versionData = try JSONDecoder().decode(VersionData.self, from: data)
-        return versionData
+        
+        return try VersionData.decode(dictionary: json)
     }
     
     private func setVersion(_ version: VersionData) {
