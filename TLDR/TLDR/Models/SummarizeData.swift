29b6469d1442e4b8bc22d0f2b1dfec2e656bebc7
유정주 <dbwjdwn9216@gmail.com>
2022-10-02 22:36:04 +0900

    Add: 원본 키워드, 요약 키워드 추가

--- TLDR/TLDR/Models/SummarizeData.swift
+++ TLDR/TLDR/Models/SummarizeData.swift
@@ -10,25 +10,36 @@ import Foundation
 struct SummarizeData {
     let text: String //원본
     let summarizeText: String //요약 내용
-    let keywords: [String] //핵심 키워드
+    let textKeywords: [String]
+    let summarizeKeywords: [String] //핵심 키워드
     
     init() {
         self.text = ""
         self.summarizeText = ""
-        self.keywords = []
+        self.textKeywords = []
+        self.summarizeKeywords = []
     }
     
-    init(text: String, summarizeText: String, keywords: [String]) {
+    init(text: String, summarizeText: String, textKeywords: [String], summarizeKeywords: [String]) {
         self.text = text
         self.summarizeText = summarizeText
-        self.keywords = keywords
+        self.textKeywords = textKeywords
+        self.summarizeKeywords = summarizeKeywords
     }
 }
 
-struct SummarizeResponseData: Decodable {
+struct SummarizeResponseData: Codable {
     let summarize: String
-    let keywords: String
+    let textKeywords: String
+    let summarizeKeywords: String
     let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case summarize
+        case textKeywords = "text_keywords"
+        case summarizeKeywords = "summarize_keywords"
+        case language
+    }
 }
 
 enum SummarizeLangauge: String {
