c38a3cc00f605e89d5100fce9afed1aebead9d2d
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 16:49:59 +0900

    미사용 코드 삭제

--- TLDR/TLDR/Application/AppDelegate.swift
+++ TLDR/TLDR/Application/AppDelegate.swift
@@ -25,12 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         //아이패드만 화면 회전 가능
         if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad {
