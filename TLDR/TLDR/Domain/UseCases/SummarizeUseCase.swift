e67db5ed5d3425e85046819b47e0683ee3ff277e
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 16:50:35 +0900

    UseCase 정의

---
+++ TLDR/TLDR/Domain/UseCases/SummarizeUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  SummarizeUseCase.swift
+//  TLDR
+//
+//  Created by 유정주 on 2023/03/20.
+//
+
+import Foundation
+
+struct SummarizeUseCase: SummarizeUseCaseProtocol {
+    let repository: SummarizeRepositoryProtocol
+    
+    init(repository: SummarizeRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func excute(requestValue: SummarizeRequestValue) async throws -> SummarizeData {
+        return try await repository.summarize(reqeustValue: requestValue)
+    }
+}
