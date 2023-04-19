ad181e800d03f1ad95930c6f10eb70482175a313
유정주 <dbwjdwn9216@gmail.com>
2023-04-19 13:57:06 +0900

    SummarizeResult 코어데이터 모델 추가

---
+++ TLDR/TLDR/Data/CoreData/CoreDataStorage.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataStorage.swift
+//  TLDR
+//
+//  Created by 유정주 on 2023/04/19.
+//
+
+import CoreData
+
+final class CoreDataStorage {
+    static let shared = CoreDataStorage()
+    
+    // MARK: - Core Data stack
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CloneStore")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                // TODO: - Log to Crashlytics
+                assertionFailure("CoreDataStorage Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+    
+    //백그라운드 thread
+    lazy var backgroundContext: NSManagedObjectContext = {
+        persistentContainer.newBackgroundContext()
+    }()
+
+    //메인 thread
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // TODO: - Log to Crashlytics
+                assertionFailure("CoreDataStorage Unresolved error \(error), \((error as NSError).userInfo)")
+            }
+        }
+    }
+
+    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        persistentContainer.performBackgroundTask(block)
+    }
+}
