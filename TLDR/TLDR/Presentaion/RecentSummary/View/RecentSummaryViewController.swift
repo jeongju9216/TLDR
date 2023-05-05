e11b58fc63509876684de49893370949a26d6612
유정주 <dbwjdwn9216@gmail.com>
2023-05-05 21:05:28 +0900

    RecentSummaryViewController 생성

---
+++ TLDR/TLDR/Presentaion/RecentSummary/View/RecentSummaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecentSummaryViewController.swift
+//  TLDR
+//
+//  Created by 유정주 on 2023/05/05.
+//
+
+import Foundation
+
+final class RecentSummaryViewController: BaseViewController<InfoView> {
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
